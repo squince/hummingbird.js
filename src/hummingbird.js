a62746d7c06d0ab85c039e4bf67f54039338a64e
Steve Quince <steve.quince@gmail.com>
2020-05-25 14:49:24 -0400

    wip - getting webpack working

---
+++ src/hummingbird.js
@@ -0,0 +1,32 @@
+const Index = require('index');
+
+(function() {
+  // # API
+  // ## Core
+  // ### constructor
+  // Convenience function for instantiating a hummingbird index
+  var hummingbird;
+
+  hummingbird = function(variantsObj) {
+    var idx;
+    return idx = new Index(variantsObj);
+  };
+
+  // ### .loggingOn
+  // Set to true or false to enable or disable logging respectively
+  // Defaults to false
+  hummingbird.loggingOn = false;
+
+  // ### .version
+  // Version of the hummingbird code base
+  hummingbird.version = "@@VERSION";
+
+  // ### .index_version
+  // Version of the index data structure
+  hummingbird.index_version = "@@INDEX_VERSION";
+
+  if (typeof module !== 'undefined' && module !== null) {
+    module.exports = hummingbird;
+  }
+
+}).call(this);
---
+++ src/utils.js
@@ -0,0 +1,82 @@
+const diacritics = require('diacritics');
+
+(function() {
+  // ## Utils
+
+  // Collection of utility functions
+  hummingbird.Utils = function() {
+    this.root = {};
+  };
+
+  // ### ::warn
+  // logs a warning message to the console
+  hummingbird.Utils.prototype.warn = function(message) {
+    if (console.warn) {
+      return console.warn(message);
+    }
+  };
+
+  // ### .debugLog
+  // logs a debug message to the console
+  hummingbird.Utils.prototype.debugLog = function(msg) {
+    return console.log(`${msg}`);
+  };
+
+  // ### .logTiming
+  // logs a message to the console preceded by time of day
+  hummingbird.Utils.prototype.logTiming = function(msg, s) {
+    var d;
+    d = new Date();
+    if (s != null) {
+      console.log(`${d.toTimeString().split(' ')[0]}.${d.getMilliseconds()} - ${msg} in ${d - s} ms`);
+    } else {
+      console.log(`${d.toTimeString().split(' ')[0]}.${d.getMilliseconds()} - ${msg}`);
+    }
+    return d;
+  };
+
+  // ### .normalizeString
+  // takes a string and normalizes it for case and diacritics
+  hummingbird.Utils.prototype.normalizeString = function(str) {
+    var re_start;
+    re_start = /^\u0002/;
+    str = diacritics.remove((str.toString()).toLowerCase());
+    str = str.replace(re_start, '');
+    return '\u0002' + str;
+  };
+
+  // ### .maxScore
+  // Returns the max score for a given string
+  hummingbird.Utils.prototype.maxScore = function(phrase, tokenizer, prefixBoost) {
+    var score;
+    score = 0;
+    if (phrase == null) {
+      return score;
+    }
+    (tokenizer.tokenize(phrase)).forEach((function(token, i, tokens) {
+      return score += this.tokenScore(token, false, prefixBoost);
+    }), this);
+    return score;
+  };
+
+  // ### .tokenScore
+  // Returns the score for the given token
+  hummingbird.Utils.prototype.tokenScore = function(token, isVariant, prefixBoost) {
+    var score;
+    if (isVariant == null) {
+      isVariant = false;
+    }
+    if (prefixBoost == null) {
+      prefixBoost = true;
+    }
+    score = token.length;
+    if (prefixBoost && token.substring(0, 1) === '\u0002') {
+      score += 0.2;
+    }
+    if (isVariant) {
+      score -= 0.4;
+    }
+    return score;
+  };
+
+}).call(this);
