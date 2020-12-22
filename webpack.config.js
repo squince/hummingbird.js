745da24f3958d7583a0245c4e9ff6a90d7b45a8d
Steve Quince <steve.quince@gmail.com>
2020-12-22 11:13:38 -0500

    move webpack output to root

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/hummingbird.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'hummingbird.js',
+    path: path.resolve(__dirname),
   },
   devServer: {
     contentBase: __dirname,
