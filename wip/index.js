bdafd4bfadc96f57858c196ce6b864c0b23db9a1
Steve Quince <steve.quince@gmail.com>
2021-01-01 21:22:32 -0500

    cleaning up deprecated and exploratory files

--- wip/index.js
+++
@@ -1,7 +0,0 @@
-export class Index {
-  constructor(variantsObj) {
-    this.createTime = new Date();
-    this.lastUpdate = null;
-    this.variantStore = variantsObj ?  {...variantsObj} : {};
-  }
-};
