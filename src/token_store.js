a361868408064d234056b7f43bfed9540d3db9c8
Steve Quince <steve.quince@gmail.com>
2021-01-05 22:42:43 -0500

    update comment syntax to jsdoc style

--- src/token_store.js
+++ src/token_store.js
@@ -10,60 +10,60 @@ export default class TokenStore {
     this.root = {};
   };
 
-  // .load
-  // Loads a previously serialized token store
+  /** .load
+  * Loads a previously serialized token store
+  */
   static load(serializedData) {
     const store = new this;
     store.root = serializedData.root;
     return store;
   }
 
-  // .toJSON
-  // Returns a representation of the token store ready for serialization.
+  /** .toJSON
+  * Returns a representation of the token store ready for serialization.
+  */
   get toJSON() {
     return {
       root: this.root
     };
   }
 
-  // .add
-  // Adds to the store a new token and document 'id', and distinguishes between variant doc matches and normal name matches.
+  /** .add
+  * Adds to the store a new token and document 'id', and distinguishes between variant doc matches and normal name matches.
+  * TODO: consider changing object keys 'n' and 'v' to more human friendly values
+  * TODO: then transform to compressed versions in .toJSON and .load methods
+  */
   add(token, isVariant, docId) {
-    if(!token) throw error('token must be supplied and is missing');
-    if(!docId) throw error('docId must be supplied and is missing');
+    if (!(token && docId)) return throw error('token and docId must both be supplied');
 
-    if (!this.root[token]) this.root[token] = {};
+    const storedToken = this.root[token] || {};
 
-    if (!isVariant) {
-      // name token matches for this document
-      if (!this.root[token]['n']) base1['n'] = {};
-      this.root[token]['n'][docId] = 1;
-    } else {
-      // variant-only token matches for this document
-      if (((ref = this.root[token]['n']) != null ? ref[docId] : void 0) == null) {
-        if ((base2 = this.root[token])['v'] == null) {
-          base2['v'] = {};
-        }
-        this.root[token]['v'][docId] = 1;
+    if (isVariant) {
+      // augment index with a variant-token
+      // only when this token is not also a name-token for this document
+      // do not store the same token as both a name-token and variant-token
+      if (!storedToken['n'] || !!storedToken['n'][docId]) {
+        if (!storedToken['v']) storedToken['v'] = {};
+        storedToken['v'][docId] = 1;
       }
-    }
-  }
-
-  // .has
-  // Checks whether this key is contained within this hummingbird.TokenStore.
-  has(token) {
-    if (!token) {
-      return false;
-    }
-    if (token in this.root) {
-      return true;
     } else {
-      return false;
+      // associate name-token match for this document
+      if (!storedToken['n']) storedToken['n'] = {};
+      storedToken['n'][docId] = 1;
     }
+    this.root[token] = storedToken;
   }
 
-  // .get
-  // Retrieve the documents for the given token
+  /** .has
+  * Checks whether this key is contained within this hummingbird.TokenStore.
+  */
+  has(token) {
+    return token ? token in this.root : false;
+  }
+
+  /** .get
+  * Retrieve the documents for the given token
+  */
   get(token, isVariant) {
     var ref, ref1, ref2, ref3;
     if (isVariant) {
@@ -73,8 +73,9 @@ export default class TokenStore {
     }
   }
 
-  // .count
-  // Number of documents associated with the given token
+  /** .count
+  * Number of documents associated with the given token
+  */
   count(token) {
     var count, ref, ref1;
     if (!token || !this.root[token]) {
@@ -86,8 +87,9 @@ export default class TokenStore {
     return count;
   }
 
-  // .remove
-  // Remove the document identified by docRef from each token in the store where it appears.
+  /** .remove
+  * Remove the document identified by docRef from each token in the store where it appears.
+  */
   remove(docRef, tokens = Object.keys(this.root)) {
     return tokens.forEach((function(token) {
       var ref, ref1;
